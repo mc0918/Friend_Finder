2202a6db8bb7ab12f7fec37cceae485673281192
mc0918 <mcampbell0918@gmail.com>
2019-04-05 15:13:51 -0500

    updated pseudocode

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,12 +2,30 @@
 
 //declare functions for finding match
 
-function findMatch(userData){
+function findMatch(userData, friends){
     const bestMatch = {
         name: '',
         photo: '',
         friendDifference: Infinity
     }
+
+
+for(let i loop over friends){
+    totalDifference = 0;
+    currentFriend = friends[i];
+
+    for (let j loop over currentFriend.scores){
+        compare userData.scores[j] and currentFriend.scores[j];
+        totalDifference += Math.abs(parseInt(userScore) - parseInt(friendScore))
+    }
+
+    if(totalDifference <= bestMatch.friendDifference){
+        bestMatch = currentFriend
+    }
+}
+
+return bestMatch;
+
 }
 
 module.exports = {1 get route for all profiles and 1 post route to add new profile}
