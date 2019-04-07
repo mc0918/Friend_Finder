6445cec13e4350cd941c15b41cbd898a5af870db
mc0918 <mcampbell0918@gmail.com>
2019-04-06 19:18:28 -0500

    populated seeds and updated schema

--- data/schema.sql
+++ data/schema.sql
@@ -6,8 +6,15 @@ USE friend_finder_db;
 CREATE TABLE profiles (
     id int(11) NOT NULL auto_increment,
     name varchar(30) NOT NULL,
-    image varchar(100) NOT NULL,
+    image varchar(500) NOT NULL,
+    description varchar(500) NOT NULL,
     scores varchar(30) NOT NULL,
     PRIMARY KEY(id) 
+);
+
+CREATE TABLE questions (
+    question_id int(11) NOT NULL auto_increment,
+    question varchar(150) NOT NULL,
+    PRIMARY KEY(question_id) 
 );
 
--- data/seed.sql
+++ data/seed.sql
@@ -1,12 +1,23 @@
-insert into profiles(name, image, scores)
-values("Chunk Biggums","...","..."),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-();
+insert into profiles(name, image,description, scores)
+values("Chunk Biggums","https://i.redd.it/jc874nohao4z.jpg","Loving father of a wonderful, smaller version of myself. Chunk II always comes first. Always looking for new connections and new friends - just keep an open mind about cloning!","2,1,2,5,5,3,5,4,3,1"),
+("Dante Reloj","https://i.imgur.com/3JOoTWR.jpg","IT expert and big DIY enthusiast. Looking for friends for one last job before we put this life of crime behind us.","5,4,1,1,2,1,4,2,1,3"),
+("Liliputia Cardimom","https://i.redd.it/e6ixgjbils4z.jpg","First nun in history to intern at Google. Love the ocean.","2,1,4,5,5,3,2,5,5,1"),
+("Macy Gray","https://i2.wp.com/riverbeats.life/neworleans/wp-content/uploads/2018/08/macy-gray-.jpg?ssl=1","Singer/songwriter/actor. Message me if you want to collab.","4,3,4,2,1,1,4,2,5,4"),
+("Osteo Porosis","https://www.demilked.com/magazine/wp-content/uploads/2018/03/5aaa1cbf0d055-funny-weird-wtf-stock-photos-3-5a392672d4110__700.jpg","I really, really need an ichthyologist. Fast. Please, I can't handle the responsibility of owning a blobfish.","5,1,3,1,4,5,5,5,2,3"),
+("Dwight 'Big America' Hartwig","https://previews.123rf.com/images/robeo/robeo1205/robeo120500330/13544771-black-african-american-american-man-sitting-on-a-lemon-and-a-lime-stitched-together.jpg","Any sewing enthusiasts out there? I have a great idea but Sprite just won't give me a chance.","1,5,1,3,4,1,2,4,2,5"),
+("Hugh Man","https://i.redd.it/70nx1d2few0y.jpg","Hello. Like other human persons I crave socialization. I enjoy eating cooked food and driving in the automotive car. Like many normal people, I know nothing is hidden at latitude 33.3943 degrees N, longitude 104.5230 degrees W.","5,1,1,3,5,5,5,5,5,1"),
+("Valkyrie Paramore IV","https://i.redd.it/1awmd6gqbt9z.jpg","Soy futurist and biohacker. Our future is with the corn. If you've got any spare venture capital you'd like to invest in a lipohilic-polycarbon-tetrafluoramine-CRISPR-Cas9 experiment hit me up.","4,3,5,1,1,2,1,1,3,3"),
+("Apple Cinnamon Instagram Copyright","https://image.shutterstock.com/z/stock-photo-close-up-of-a-stylish-hipster-woman-in-sunglasses-look-to-the-camera-and-make-silence-sign-with-587022575.jpg","$500.00 per message. $899.00 for an instagram shoutout. Will eat at your restaurant for exposure. Looking for downstream investors in my inverse triangle reverse-managed product funnel enterprise.","5,1,5,5,1,3,3,1,2,1"),
+("Yajaira Manjarrez","https://i.redd.it/76xjma1wsxlz.jpg","If you think having the goal of perfect synergy between hands and feet is weird we won't like each other. Career-oriented and always on the lookout for my next big opportunity.","3,5,2,2,4,3,4,3,5,5");
 
+insert into questions(question)
+values("Are you adventurous?"),
+("Do you enjoy spicy foods?"),
+("Would you be excited if the government contacted you about a secret mission only you can do?"),
+("If you were locked in a museum overnight, how scared would you be?"),
+("If a bag full of $100 bills dropped in your lap, would you be suspicious?"),
+("Are you enthusiastic about parrot-ownership?"),
+("Would you be interested in seeing my vintage 1967 Corvette Stingray?"),
+("You're up in the mountains, how scared are you of the edge of the cliff?"),
+("You wake up to a strange man at the foot of your bed screaming at you about missing the shareholder meeting, are you surprised?"),
+("When the working class rises up to reclaim the fruits of their labor, are you likely to join them?");
